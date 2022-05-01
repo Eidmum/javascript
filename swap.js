e61aa28305fe16a4b0ae9c6f90e62d3427ae3147
Eidmum <64984252+Eidmum@users.noreply.github.com>
2022-05-02 01:14:58 +0600

    Create swap.js

---
+++ swap.js
@@ -0,0 +1,16 @@
+var a=7,b=10,temp;
+console.log("before swap: a =",a,"b =",b);
+temp=a;
+a=b;
+b=temp;
+console.log("after swap: a =",a,"b =",b);      
+var x=1,y=2;
+console.log("before swap: x =",x,"y =",y);  
+x=x+y;  
+y=x-y;
+x=x-y;
+console.log("after swap: x =",x,"y =",y);  
+let p=12,q=13;
+[p,q]=[q,p];
+
+console.log("after swap: p=",p,"q =",q);  
